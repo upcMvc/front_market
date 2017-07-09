df88149f60e51e4820c32d8985f88c386ac38526
zhangfuli <1171067930@qq.com>
2017-07-09 10:42:35 +0800

    导航栏组件

---
+++ src/components/Nav.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="nav">
+    <div id="wrapper" v-bind:class="{'toggled':isClosed}">
+      <!-- Sidebar -->
+      <nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
+        <ul class="nav sidebar-nav">
+          <li>
+            <a href="#"><i class="fa fa-fw fa-home"></i>主页</a>
+          </li>
+          <li>
+            <a href="#"><i class="fa fa-fw fa-folder"></i> Page one</a>
+          </li>
+          <li>
+            <a href="#"><i class="fa fa-fw fa-file-o"></i> Second page</a>
+          </li>
+          <li>
+            <a href="#"><i class="fa fa-fw fa-cog"></i> Third page</a>
+          </li>
+
+          <li>
+            <a href="#"><i class="fa fa-fw fa-bank"></i> Page four</a>
+          </li>
+          <li>
+            <a href="#"><i class="fa fa-fw fa-dropbox"></i> Page 5</a>
+          </li>
+          <li>
+            <a href="#"><i class="fa fa-fw fa-twitter"></i> Last page</a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /#sidebar-wrapper -->
+      <!-- Page Content -->
+      <div id="page-content-wrapper">
+        <button type="button" class="hamburger animated fadeInLeft" v-bind:class="{'is-closed':!isClosed,'is-open':isClosed}" @click="hamburger_cross()">
+          <span class="hamb-top"></span>
+          <span class="hamb-middle"></span>
+          <span class="hamb-bottom"></span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import $ from 'jquery'
+  import '../../node_modules/bootstrap/dist/js/bootstrap'
+  export default {
+    name: 'nav',
+    data () {
+      return {
+        isClosed: false,  //判断左侧导航栏的开关
+        overlay:''
+      }
+    },
+    created(){
+        this.myInit()
+    },
+    methods : {
+      myInit(){
+        this.overlay = $('.overlay')
+      },
+      hamburger_cross(){
+        if (this.isClosed == true) {
+          this.overlay.hide()
+          this.isClosed = false;
+        } else {
+          this.overlay.show()
+          this.isClosed = true
+        }
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  @import '../assets/silder/css/style.css';
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import Nav from '@/components/Nav'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    },
+    //侧边栏做测试用
+    {
+      path: '/nav',
+      name: 'av',
+      component: Nav
+    }
+  ]
+})

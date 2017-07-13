49edb672c29f254fc4dd1b19eb7c3b2688b6b2f5
haojun wang <549180928@qq.com>
2017-07-13 15:41:52 +0800

    完成浏览历史记录  不过貌似不用了

--- src/components/History.vue
+++ src/components/History.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="history">
-   <ul v-for="h in history">
-     <li>{{h.goodId}}</li>
-     <li>{{h.goodName}}</li>
-     <li>{{h.imgPath}}</li>
-     <li>{{h.time}}</li>
-     <li>{{h.userId}}</li>
-     <li>{{h.price}}</li>
-
-
-     <li>history</li>
-
-   </ul>
+    <Navs></Navs>
+    <div class="product" v-for="h in history">
+      <div class="make3D">
+        <img src="../assets/good/img/8.jpg" alt="">
+        <div class="stats">
+          <div class="stats-container">
+            <span class="product_price">¥{{ h.price }}</span>
+            <span class="product_name">{{ h.goodName }}</span>   <!--加入商品详细信息-->
+            <span></span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,6 +20,7 @@
   import $ from 'jquery'
   import BScroll from 'better-scroll'
   import API from '../config/req'
+  import Navs from './Nav'
   export default {
     name: 'history',
     data () {
@@ -56,14 +58,58 @@
         },()=>{
             console.log("delete error")
         })
+      },
+      initScroll() {
+        this.foodsScroll = new BScroll(this.$refs.foodsWrapper);
+      },
+      initShowing(){
+        $('.product').each(function (i, el) {
+          $(el).find('.make3D').hover(function () {
+            $(this).parent().css('z-index', '20');
+            $(this).addClass('animate');
+            $(this).find('div.carouselNext, div.carouselPrev').addClass('visible');
+          }, function () {
+            $(this).removeClass('animate');
+            $(this).parent().css('z-index', '1');
+            $(this).find('div.carouselNext, div.carouselPrev').removeClass('visible');
+          });
+        });
       }
-
+    },
+    components:{
+        Navs
     }
+
   }
 </script>
 
 
 <style scoped>
   @import url('../assets/good/css/styles.css');
+
+  .stats-container {
+    width: 324px;
+    height: 359px;
+  }
+
+  .shopCar {
+    background: white;
+    background: url('../assets/img/shopcar.svg') no-repeat 0 0;
+    background-position: -5px;
+    width: 60px;
+    height: 60px;
+    box-shadow: 0.5px 0.5px 0.5px 0.5px #888888;
+    border-radius: 5px;
+    position: fixed;
+    bottom: 22px;
+    left: 32px;
+    z-index: 100;
+
+  }
+
+  .shopCar:hover {
+    box-shadow: 1px 1px 1px 1px #888888;
+    transition: all 0.5s;
+  }
 
 </style>
